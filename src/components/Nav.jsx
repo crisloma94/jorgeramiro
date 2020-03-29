8ae2e7e99742893968e7a36cce0742aa8331cae5
Cristina López Martín <cristinalopezmartin@MacBook-Pro-de-Cristina.local>
2020-03-29 20:21:05 +0200

    Add social media icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./../resources/styles/Nav.scss";
+import linkedinIcon from "./../resources/images/icons/linkedin.svg";
+import behanceIcon from "./../resources/images/icons/behance.svg";
+import instagramIcon from "./../resources/images/icons/instagram.svg";
 
 class Nav extends Component {
   state = {};
@@ -23,21 +26,27 @@ class Nav extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <li>Linkedin</li>
+              <li>
+                <img src={linkedinIcon} alt="linkedin icon" />
+              </li>
             </a>
             <a
               href="https://www.behance.net/JorgeRamiro"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <li>Behance</li>
+              <li>
+                <img src={behanceIcon} alt="behance icon" />
+              </li>
             </a>
             <a
               href="https://www.instagram.com/ramiro.jorge/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <li>Instagram</li>
+              <li>
+                <img src={instagramIcon} alt="instagram icon" />
+              </li>
             </a>
           </ul>
         </ul>
