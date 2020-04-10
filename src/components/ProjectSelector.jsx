8f5146afe4841557bf967019dc5909a94260b392
Cristina López Martín <cristinalopezmartin@MacBook-Pro-de-Cristina.local>
2020-04-10 23:48:31 +0200

    use scroll with hooks

--- src/components/ProjectSelector.jsx
+++ src/components/ProjectSelector.jsx
@@ -1,36 +1,56 @@
-import React, { Component } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./../resources/styles/ProjectSelector.scss";
 
-class HeroPresentation extends Component {
-  render() {
-    const projects = this.props.projects;
-    console.log("public_url", process.env.PUBLIC_URL);
-    return (
-      <section className="project-selector">
-        <ul className="project-list">
-          {projects.map((project, index) => (
-            <li
-              key={project.id}
-              className={`project-link ${index === 0 ? "selected" : ""} ${
-                index === 2 ? "last" : ""
-              }`}
-            >
-              <Link to={`/projects/${project.id}`}>
-                {`${(index + 1).toString().padStart(2, "0")}_${
-                  project.data.name
-                }`}
-              </Link>
-              <img
-                src={`${process.env.PUBLIC_URL}${project.data.mainImage}`}
-                alt={project.data.name}
-              />
-            </li>
-          ))}
-        </ul>
-      </section>
-    );
-  }
-}
+const ProjectSelector = props => {
+  const projects = props.projects;
+  const projectSelector = useRef();
 
-export default HeroPresentation;
+  const [active, setActive] = useState(0);
+
+  const [position, setPosition] = useState(
+    document.body.getBoundingClientRect().y
+  );
+
+  const handleScroll = useCallback(() => {
+    const newPosition = projectSelector.current.getBoundingClientRect().y;
+    if (position - newPosition > 40) {
+      setActive((active + 1) % projects.length);
+      setPosition(newPosition);
+    } else if (position - newPosition < -40) {
+      setActive((active - 1) % projects.length);
+      setPosition(newPosition);
+    }
+    console.log("difference: ", position - newPosition);
+  }, [active, position, projects.length]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  return (
+    <section id="project-selector" ref={projectSelector}>
+      <ul className="project-list">
+        {projects.map((project, index) => (
+          <li
+            key={project.id}
+            className={`project-link ${active === index ? "active" : ""}`}
+          >
+            <Link to={`/projects/${project.id}`}>
+              {`${(index + 1).toString().padStart(2, "0")}_${
+                project.data.name
+              }`}
+            </Link>
+            <img
+              src={`${process.env.PUBLIC_URL}${project.data.mainImage}`}
+              alt={project.data.name}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectSelector;
