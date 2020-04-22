e8f86c290e3d40ffed58a29df8c972de6cbf565f
Cristina López Martín <cristina.lopez.martin@everis.com>
2020-04-22 10:39:00 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ec3d5484aef436a3de46fae0a6d07479.js"
+  "./precache-manifest.843066e8ea245c7fca1255cd5edb7691.js"
 );
 
 self.addEventListener('message', (event) => {
